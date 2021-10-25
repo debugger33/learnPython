6188ad82fbd33fe398417583fce74a6791d4b3d0
debugger33 <heinformatika@gmail.com>
2021-10-25 21:26:08 +0700

    commit

---
+++ dataCamp/goToTheMoon.py
@@ -0,0 +1,11 @@
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import pi
+from math import radians
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = (radians(12) * r)
+
+# Print out dist
+print(dist)
