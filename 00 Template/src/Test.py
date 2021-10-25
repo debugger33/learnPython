61ad899c21b75f782a1ec1acb3528e14087b4bb0
debugger33 <heinformatika@gmail.com>
2021-10-25 15:42:25 +0700

    commit

---
+++ 00 Template/src/Test.py
@@ -0,0 +1,14 @@
+import time
+start_time = time.time()
+
+print("Hallo Dunia")
+print("Test")
+
+"""comment multi line
+bisa kebawah"""
+a=100
+print(a)
+
+print(time.time() - start_time, "detik")
+"""Cara Compile
+python -m py_compile Main.py"""
---
+++ 01 Variable/Main.py
@@ -0,0 +1,12 @@
+#Variable adalah tempat menyimpan data
+
+# Menaruh / assigment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
